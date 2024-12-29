77559abcd54433965866b8c50592f3cd37ae0e95
Wei Wang <wvw@google.com>
2024-12-30 00:11:38 +0100

    a71-common: Move PowerHAL into Pixel common
    Bug: 129495482
    Bug: 111098032
    Test: Boot and check PowerHAL
    Change-Id: I79b7276ff0c28885877af8b0533fe1f9d81b3b0c

--- common.mk
+++ common.mk
@@ -307,11 +307,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti.sm6150 \
+    android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
-
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/power/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
@@ -389,6 +386,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     bootable/deprecated-ota \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/samsung \
     vendor/qcom/opensource/usb/etc
