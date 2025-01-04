03767fa518005075eb0a89746a29dc403d5d9703
Alexander Koskovich <zvnexus@outlook.com>
2025-01-04 21:29:27 +0100

    a71-common: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,8 +154,7 @@ BOARD_KERNEL_CMDLINE := \
     nokaslr \
     printk.devkmsg=on \
     service_locator.enable=1 \
-    swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
+    swiotlb=1
 
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
