049e27f7ca910fab4d5a3bd71f21b5777f38e1ed
Haky86 <hakamelassouad@gmail.com>
2025-01-19 19:55:12 +0100

    a71-common: Drop unused WCNSS symlink
    Change-Id: Ia503dfd1b3658d369b42d9181052726efceace35

--- Android.mk
+++ Android.mk
@@ -39,13 +39,4 @@ $(MODEM_MOUNT_POINT): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(DSP_MOUNT_POINT) $(FIRMWARE_MOUNT_POINT) $(MODEM_MOUNT_POINT)
 
-WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin
-$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "WCNSS MAC bin link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_MAC_SYMLINK)
-
 endif
