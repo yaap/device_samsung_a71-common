LineageOS/android_device_samsung_a71-common

a71-common: init: Set soc properties
Set soc model and manufacturer properties

Change-Id: Ieee34b7bfab87b241abc5ed3318b46a5c6f93456

common.mk
@@ -328,6 +328,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
+    init.qti.early_init.sh \
 
 # Recovery
 AB_OTA_UPDATER := false

yaap/device_samsung_a71-common

common.mk
@@ -327,7 +327,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
-    init.qti.chg_policy.sh
+    init.qti.chg_policy.sh \
+    init.qti.early_init.sh
 
 # Recovery
 AB_OTA_UPDATER := false